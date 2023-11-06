6c000d51400a6c32d4a7860ce7ae5132bd36b1cd
unknown <matthewlin@outlook.com>
2023-11-06 00:37:40 -0500

    initial manage event

---
+++ frontend/src/components/Manage/AttendeeBox.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Card from '@mui/material/Card';
+import Typography from '@mui/material/Typography';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Grid from '@mui/material/Grid';
+
+const AttendeeBox = ({attendeeName}) => {
+  return (
+    <Card variant="outlined">
+    <CardContent>
+      <Grid container spacing={2}>
+        <Grid container item xs={6} spacing={2}>
+          <Grid item xs={14}>
+            <Typography variant="body1">{attendeeName}</Typography>
+            <Typography variant="body1">Yo</Typography>
+          </Grid>
+        </Grid>
+        <Grid container item xs={12} justifyContent="flex-end">
+          <Button variant="contained" color="primary">
+            Remove
+          </Button>
+        </Grid>
+      </Grid>
+    </CardContent>
+  </Card>
+  );
+};
+
+export default AttendeeBox;
